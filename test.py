c800d2f66a70d1e780bc14733170863939b79695
jatrozky <jaratrozky@mail.ru>
2020-08-23 18:48:01 +0300

    second

--- test.py
+++ test.py
@@ -4,9 +4,16 @@ app = Flask(__name__)
 
 @app.route('/resume')
 @app.route('/')
-def hello_world():
+def showResume():
     return render_template('resume.html')
+
+def showAbout():
+    return render_template('about.html')
+
+
+def showInterests():
+    return render_template('interests.html')
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True	)
